2dd016a1abf0cb48d66d388aa516a712207aed7d
BuildTools <justinkrasner98@gmail.com>
2020-04-23 17:32:30 -0400

    .

---
+++ assignment11/src/Date.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const today  = new Date();
+const options = {weekday: 'long', year: 'numeric', month: 'long', day: 'numeric'};
+var td = today.toLocaleDateString("en-US", options);
+
+export default function _Date_() {
+
+    return (
+        <div className="Date">
+            Last updated {td} 16:00 CET
+        </div>
+    ) 
+}
